fc92bf3bea9783266125933d6796697041d3c926
Aldi Satria <52058660+acvn@users.noreply.github.com>
2021-04-17 14:05:08 +0700

    Create crawler.sh

---
+++ crawler.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+(while read url ; do gau $url &&  waybackurls $url && hakrawler -url $url -plain -depth 3; done < "$1" ) | sort -u | tee crawl-an | httpx -title -content-length -status-code -silent -content-type -no-color | awk '{ t = $1; $1 = $2; $2 = t; print; }' | awk '{ t = $3; $3 = $2; $2 = t; print; }' | sort -u > httpx-an
