75c230d636f55c2ac96a482472ff2e1ede7aa1e8
acvn <aldisatria1996@gmail.com>
2022-01-13 20:31:49 +0700

    asd

--- cekport.sh
+++ cekport.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-sudo ~/masscan/bin/masscan -p1-65535,U:1-65535 $(dig +short $1) --rate=1000 -e en0
+sudo ~/masscan/bin/masscan -p1-65535,U:1-65535 $(dig +short $1) --rate=1000 -e en0 > port-$1 
+sudo nmap -Pn -f -sV $1 > port-service-$1
+sudo nmap -sT -A -O -v -Pn $1 > port-os-$1
